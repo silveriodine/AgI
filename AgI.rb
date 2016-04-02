87d92e43af9afa6e4a125234d6d2f15b3efac7a4
Silveriodine <silveriodine@users.noreply.github.com>
2016-04-02 01:46:26 -0400

    Move the Qcow2 creation command logic over into the instance disk test.
    Move the logic for creating the qcow along with the command string
    construction into the test structure for the instance disk itself.

--- AgI.rb
+++ AgI.rb
@@ -270,17 +270,18 @@ instances.each { | instancecur |
     end
 
     #QCOW generation block. If qcowback is nil, we do nothing. Otherwise we proceed throug the routine
-    qemuimgcreate = nil
     if instancecur['qcowback']
-    qemuimgcreate = "qemu-img create -q -f qcow2 -o backing_file=\"#{instancecur['qcowback']}\" \
+		#create the qemu-img clone command in a string
+		qemuimgcreate = "qemu-img create -q -f qcow2 -o backing_file=\"#{instancecur['qcowback']}\" \
 \"#{instancecur['outdir']}/#{instancecur['metadata']['instance-id']}.qcow2\" #{instancecur['qcowsize']} #{$cmdoutput}"
-    debug( 2, "QCOW2 creation command: #{qemuimgcreate}" )
-    end
+		debug( 2, "QCOW2 creation command: #{qemuimgcreate}" )
     
-    if qemuimgcreate and system( qemuimgcreate )
-	debug( "QCOW2 generation for \"#{instancecur['metadata']['instance-id']}\ at size \"#{instancecur['qcowsize']}\" successful!" )
-    else
-	errexit( "Failure during QCOW2 disk generation for \"#{instancecur['metadata']['instance-id']}\"" )
+		#then run the qemu-img command
+		if system( qemuimgcreate )
+			debug( "QCOW2 generation for \"#{instancecur['metadata']['instance-id']}\ at size \"#{instancecur['qcowsize']}\" successful!" )
+		else
+			errexit( "Failure during QCOW2 disk generation for \"#{instancecur['metadata']['instance-id']}\"" )
+		end
     end
     
     if $printstyle == 'names'
